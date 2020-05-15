6b59b089ef96c0c46b204af852bc78f0c4f8cd12
Callet91 <calle.jensen@outlook.com>
2020-05-15 08:29:33 +0000

    Fixing graphics card allocation problem on GCP

--- mains/main_alexnet_modded_filter.py
+++ mains/main_alexnet_modded_filter.py
@@ -1,10 +1,25 @@
 """Main for alexnet modded v.1."""
+import tensorflow as tf
 from data_loader.data_load import Dataset
 from models.alexnet_modded_filter import AlexNetModded
 
 
 def main():
     """Main."""
+
+    gpus = tf.config.experimental.list_physical_devices("GPU")
+    if gpus:
+        # Restrict TensorFlow to only allocate 1GB of memory on the first GPU
+        try:
+            tf.config.experimental.set_virtual_device_configuration(
+                gpus[0],
+                [tf.config.experimental.VirtualDeviceConfiguration(memory_limit=14336)],
+            )
+            logical_gpus = tf.config.experimental.list_logical_devices("GPU")
+            print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPUs")
+        except RuntimeError as error:
+            # Virtual devices must be set before GPUs have been initialized
+            print(error)
 
     # Load configuration file
     config = open("/workspaces/DD2424-project/configs/alexnet_modded_config.json")
