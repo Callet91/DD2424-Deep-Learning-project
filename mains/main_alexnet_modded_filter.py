fe0e19e981e50ad6c72ca39e26658671a8f67cd0
Callet91 <calle.jensen@outlook.com>
2020-05-15 08:34:53 +0000

    Adding prints

--- mains/main_alexnet_modded_filter.py
+++ mains/main_alexnet_modded_filter.py
@@ -23,22 +23,30 @@ def main():
 
     # Load configuration file
     config = open("/workspaces/DD2424-project/configs/alexnet_modded_config.json")
+    print("Config loaded...")
 
     # Create Dataset of tiny-imagenet from config
     dataset = Dataset(config)
+    print("Dataset created...")
 
     # Get training data and validation data
     ds_train = dataset.get_data("train")
     ds_test = dataset.get_data("val")
 
+    print("Data extracted...")
     config_alex = open("/workspaces/DD2424-project/configs/alexnet_modded_config.json")
 
     # Train pure alexnet according to configuration file
     alex = AlexNetModded(config_alex)
+    print("Model created...")
+
     alex.set_train_data(ds_train)
     alex.set_test_data(ds_test)
+    print("Data is set to model...")
 
     alex.generate_model()
+    print("Model generated...")
+
     alex.summary()
     alex.start_train()
 
